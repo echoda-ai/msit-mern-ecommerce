9f9126af669d3356ee711872b163b11a667c6ff6
SEANGLAY <devolutionary.wizard@gmail.com>
2024-11-19 22:47:57 +0700

    update tsconfig

--- backend/build/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_SECRET = exports.MONGODB_URI = exports.PORT = void 0;
-const get_env_1 = require("./config/get-env");
-exports.PORT = (0, get_env_1.getEnv)("PORT");
-exports.MONGODB_URI = (0, get_env_1.getEnv)("MONGODB_URI");
-exports.JWT_SECRET = (0, get_env_1.getEnv)("JWT_SECRET");
